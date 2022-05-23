f13950029b8e02e188f9be116cbd38d30bb42f56
AmilcarFerreira <amilcar.ferreira.easysoft.com.br>
2022-05-23 17:29:16 -0300

    Validação de Usuário

--- api/src/modelos/Usuario.go
+++ api/src/modelos/Usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario representa um usuário da Rede Social
 type Usuario struct {
@@ -10,4 +14,40 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+// Preparar vai validar e formatar o usuário recebido
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O Nome é obrigatório!")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O Nick é obrigatório!")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O E-mail é obrigatório!")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("A Senha é obrigatória!")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
