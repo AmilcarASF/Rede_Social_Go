c3b7496d48342ef6862ed181e5ba7c39054de1a6
AmilcarFerreira <amilcar.ferreira.easysoft.com.br>
2022-05-30 15:40:55 -0300

    Crud Publicações

---
+++ api/src/repositorios/publicacoes.go
@@ -0,0 +1,152 @@
+package repositorios
+
+import (
+	"api/src/modelos"
+	"database/sql"
+)
+
+// Publicacoes representa um repositorio de publicacoes
+type publicacoes struct {
+	db *sql.DB
+}
+
+// NovorepositorioDePublicacoes cria um repositorio de publicacoes
+func NovoRepositorioDePublicacoes(db *sql.DB) *publicacoes {
+	return &publicacoes{db}
+}
+
+// Criar insere uma publicacao no banco de dados
+func (repositorio publicacoes) Criar(publicacao modelos.Publicacao) (uint64, error) {
+	statement, erro := repositorio.db.Prepare(
+		"insert into publicacoes (titulo, conteudo, autor_id) values (?, ?, ?)",
+	)
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(publicacao.Titulo, publicacao.Conteudo, publicacao.AutorID)
+	if erro != nil {
+		return 0, erro
+	}
+
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIDInserido), nil
+}
+
+// BuscarPorID traz uma única publicaçaõ do banco de dados
+func (repositorio publicacoes) BuscarPorID(publicacaoID uint64) (modelos.Publicacao, error) {
+	linha, erro := repositorio.db.Query(`
+		select p.id,
+		       p.titulo,
+			   p.conteudo,
+			   p.autor_id,
+			   p.curtidas,
+			   p.criadaEm,
+		       u.nick
+		  from publicacoes p
+		 inner join usuarios u on u.id = p.autor_id
+		 where p.id = ? 	   
+	`, publicacaoID)
+	if erro != nil {
+		return modelos.Publicacao{}, erro
+	}
+	defer linha.Close()
+
+	var publicacao modelos.Publicacao
+
+	if linha.Next() {
+		if erro = linha.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadaEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return modelos.Publicacao{}, erro
+		}
+	}
+
+	return publicacao, nil
+}
+
+// Buscar traz as publicaçõrs dos usuários seguidos e também do próprio usuário que fez a requisição
+func (repositorio publicacoes) Buscar(usuarioID uint64) ([]modelos.Publicacao, error) {
+	linhas, erro := repositorio.db.Query(`
+		select distinct p.id,
+		       p.titulo,
+		       p.conteudo,
+		       p.autor_id,
+		       p.curtidas,
+		       p.criadaEm,
+		       u.nick
+          from publicacoes p
+         inner join usuarios u on u.id = p.autor_id
+         inner join seguidores s on p.autor_id  = s.usuario_id 
+         where u.id = ? or s.seguidor_id  = ?
+		 order by p.id desc`,
+		usuarioID, usuarioID,
+	)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var publicacoes []modelos.Publicacao
+
+	for linhas.Next() {
+		var publicacao modelos.Publicacao
+
+		if erro = linhas.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadaEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return nil, erro
+		}
+
+		publicacoes = append(publicacoes, publicacao)
+	}
+
+	return publicacoes, nil
+}
+
+// Atualizar altera os dados de uma publicacao no banco de dados
+func (repositorio publicacoes) Atualizar(publicacaoID uint64, publicacao modelos.Publicacao) error {
+	statement, erro := repositorio.db.Prepare("update publicacoes set titulo =?, conteudo = ? where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacao.Titulo, publicacao.Conteudo, publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+// Deletar exclui um apublicacao do banco de dados
+func (repositorio publicacoes) Deletar(publicacaoID uint64) error {
+	statement, erro := repositorio.db.Prepare("delete from publicacoes where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
