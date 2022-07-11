e3b1d5077a6ef78f245bc542a00c1b8b2718b3e8
AmilcarFerreira <amilcar.ferreira.easysoft.com.br>
2022-07-11 14:27:58 -0300

    Curtir publicação

--- webapp/assets/js/publicacoes.js
+++ webapp/assets/js/publicacoes.js
@@ -18,5 +18,23 @@ function criarPublicacao(evento) {
 }
 
 function curtirPublicacao(evento) {
-    alert("curtirPublicacao");   
+    evento.preventDefault(); 
+
+    const elementoClicado = $(evento.target);
+    const publicacaoId = elementoClicado.closest('div').data('publicacao-id');
+
+    elementoClicado.prop('disabled', true);
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}/curtir`,
+        method: "POST"
+    }).done(function() {
+        const contadorDeCurtidas = elementoClicado.next('span');
+        const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+        contadorDeCurtidas.text(quantidadeDeCurtidas + 1);
+    }).fail(function() {
+        alert("Erro ao curtir a publicação!");
+    }).always(function() {
+        elementoClicado.prop('disabled', false);
+    });  
 }
