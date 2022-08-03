fd8885124a20bbe21a257938360f3e2a14a3df09
AmilcarFerreira <amilcar.ferreira.easysoft.com.br>
2022-08-03 16:45:37 -0300

    Página do perfil

--- webapp/src/modelos/Usuario.go
+++ webapp/src/modelos/Usuario.go
@@ -1,6 +1,9 @@
 package modelos
 
-import "time"
+import (
+	"net/http"
+	"time"
+)
 
 // Usuario representa uma pessoa utiliando a aplicação
 type Usuario struct {
@@ -12,4 +15,33 @@ type Usuario struct {
 	Seguidores  []Usuario    `json:"seguidores"`
 	Seguindo    []Usuario    `json:"seguindo"`
 	Publicacoes []Publicacao `json:"publicacoes"`
+}
+
+// BuscarUsuarioCompleto faz 4 requisições na API para montar o usuário
+func BuscarUsuarioCompleto(usuarioID uint64, r *http.Request) (Usuario, error) {
+	canalUsuario := make(chan Usuario)
+	canalSeguidores := make(chan []Usuario)
+	canalSeguindo := make(chan []Usuario)
+	canalPublicacoes := make(chan []Publicacao)
+
+	go BuscarDadosDoUsuario(canalUsuario, usuarioID, r)
+	go BuscarSeguidores(canalSeguidores, usuarioID, r)
+	go BuscarSeguindo(canalSeguindo, usuarioID, r)
+	go BuscarPublicacoes(canalPublicacoes, usuarioID, r)
+}
+
+func BuscarDadosDoUsuario(canal <-chan Usuario, usuarioID uint64, r *http.Request) {
+
+}
+
+func BuscarSeguidores(canal <-chan []Usuario, usuarioID uint64, r *http.Request) {
+
+}
+
+func BuscarSeguindo(canal <-chan []Usuario, usuarioID uint64, r *http.Request) {
+
+}
+
+func BuscarPublicacoes(canal <-chan []Publicacao, usuarioID uint64, r *http.Request) {
+
 }
